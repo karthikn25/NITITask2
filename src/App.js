54bdbf8493807c8fe06c3866390b505890754803
karthikn25 <mkarthikeyan179@gmail.com>
2024-03-18 22:20:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import image from './Images/logo.png';
+
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar">
+    <div className="logo"><img src={image} alt='logo'/></div>
+    <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+      <ul>
+        <li><i class='bx bx-home'></i>Home</li>
+        <li><i class='bx bx-user'></i>About</li>
+        <li><i class='bx bx-briefcase' ></i>Services</li>
+        <li><i class='bx bxs-contact' ></i>Contact</li>
+      </ul>
+    </div>
+    
+    <div className="menu-icon" onClick={toggleMenu}>
+      <span></span>
+      <span></span>
+      <span></span>
+       </div>
+       
+  </nav>
+ { /*<div className='mobile-menu' style={{display:isMenuOpen ? "none" : "block"}}>
+       <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+  </div>*/}
     </div>
   );
 }
